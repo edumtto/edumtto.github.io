f9947871ee6aa1a70e0f1fb537af777374184341
Eduardo Oliveira <edumottaoliveira@gmail.com>
2026-01-04 19:22:32 -0800

    Remove old files

--- versions/v0.5/setTheme.js
+++
@@ -1,8 +0,0 @@
-function setThemeColor() {
-  var r = document.querySelector(':root');
-  r.style.setProperty('--accent', localStorage.getItem('accent'))
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  setThemeColor();
-});
